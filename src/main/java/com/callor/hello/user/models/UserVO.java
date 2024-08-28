bc45a705cdc59b482cc1bd83d86758e4b7384d55
inhoo123 <xkdns50@naver.com>
2024-08-28 15:33:49 +0900

    sec

---
+++ src/main/java/com/callor/hello/user/models/UserVO.java
@@ -0,0 +1,22 @@
+package com.callor.hello.user.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserVO {
+
+	private String username;
+	private String passoword;
+	private String email;
+	private String name;
+}
